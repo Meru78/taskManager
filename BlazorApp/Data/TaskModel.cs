16c7327a2ee4b69c581e42ba07c7430da696b250
Meru78 <melnikov021@gmail.com>
2022-06-11 22:09:55 +0300

    123

---
+++ BlazorApp/Data/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp.Data
+{
+    public class TaskModel
+    {
+        public int Id {get; set;}        
+        public String Detail { get; set; }
+        public DateTime DateTime { get; set; }
+        public TaskStatuses Status { get; set; }
+
+    }
+
+    public enum TaskStatuses
+    {
+        ToDo,
+        Started,
+        Done
+    }
+}
